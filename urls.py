5dc2aef5a43e0b100d150043b37b1ed723ebc99d
Surov Alexey <zavsnar@gmail.com>
2013-10-04 17:46:29 +0400

    delete files from index

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-from views import create_user_by_invite
-
-urlpatterns = patterns('',
-    url(r'^invite/(?P<hash_key>.+)/$', 'create_user_by_invite', name='create_user_by_invite'),
-    )
